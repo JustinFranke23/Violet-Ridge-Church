e1d9049cef6b44fad3f30d808ad48b56db3afa87
Justin Franke <justinfranke23@gmail.com>
2022-11-16 23:22:06 -0500

    nearly finished all javascript for calendar.

--- javascript/script.js
+++ javascript/script.js
@@ -19,9 +19,89 @@ const validateForm = () => {
     }
 }
 
-//Map API
+//Calendar
 
-var map = L.map('map').setView([38.79462555473118, -84.6169389355239], 19);
+const date = new Date();
+
+const renderCalendar = () => {
+    date.setDate(1);
+
+    const monthDays = document.querySelector(".days");
+
+    const lastDay= new Date(date.getFullYear(),date.getMonth() + 1, 0).getDate();
+
+    const prevLastDay = new Date(date.getFullYear(),date.getMonth(),0).getDate();
+
+    const firstDayIndex = date.getDay();
+
+    const lastDayIndex = new Date(date.getFullYear(),date.getMonth() + 1, 0).getDay();
+
+    const nextDays = 7 - lastDayIndex - 1;
+
+    const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+    ];
+
+    document.querySelector(".date h1").innerHTML = months[date.getMonth()];
+
+    document.querySelector(".date p").innerHTML = new Date().toDateString();
+
+    let days = "";
+
+
+    for (let x = firstDayIndex; x > 0; x--) {
+    days += `<div class="prev-date">${prevLastDay - x + 1}</div>`;
+ }
+
+    for (let i = 1; i <= lastDay; i++) {
+     if ( i === new Date().getDate() && date.getMonth() === new Date().getMonth()
+     )
+    {
+        days += `<div class="today">${i}</div>`;
+    } else {
+        days += `<div>${i}</div>`;
+    }
+    
+   }
+
+  for (let j = 1;j <= nextDays;j++) {
+    days += `<div class="next-date">${j}</div>`;
+    monthDays.innerHTML = days;
+   }
+};
+
+
+
+document.querySelector(".prev").addEventListener("click", () => {
+    date.setMonth(date.getMonth() - 1);
+    renderCalendar();
+});
+
+document.querySelector(".next").addEventListener("click", () => {
+    date.setMonth(date.getMonth() + 1);
+    renderCalendar();
+});
+
+renderCalendar();
+
+
+
+//Map API will attempt to fix console error
+
+const L = window.L
+
+const map = L.map('map').setView([38.79462555473118, -84.6169389355239], 19);
 L.tileLayer('https://maptiles.p.rapidapi.com/en/map/v1/{z}/{x}/{y}.png?rapidapi-key=53c4f0644bmsh7a490ca7886f56ep180f41jsnac2c5504b031', {
 attribution: 'Tiles &copy: <a href="https://www.maptilesapi.com/">MapTiles API</a>, Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 maxZoom: 19
